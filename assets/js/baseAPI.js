e955bb19e2248c1d8acdfeba8d34193e2b471408
HuangXi <2653560192@qq.com>
2021-03-15 13:30:32 +0800

    index 页面完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -12,4 +12,24 @@ $.ajaxPrefilter(function (options) {
     // console.log(options);
     // 拼接对应环境的服务器地址
     options.url = baseURL + options.url;
-})
+
+    // 身份验证
+    if (options.url.indexOf('/my/') !== -1) {
+        options.header = {
+            Authorization: localStorage.getItem('token') || '',
+        }
+    };
+
+
+    // 拦截所有响应，判断身份验证信息
+    params.complete = function (res) {
+        console.log(res.responseJSON);
+        let obj = res.responseJSON;
+        if (obj.status == 1 && obj.messge == '身份验证失败！') {
+            // 清空本地token
+            localStorage.removeItem('token');
+            // 页面跳转
+            location.href = '/login.html';
+        }
+    }
+});
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,20 @@ $(function () {
     // 调用函数
     getUserInfo()
 
+    // 点击退出按钮
+    let layer = layui.layer;
+    $('#btnLogout').on('click', function () {
+        // 框架提供的询问框
+        layer.confirm('是否确认退出？', { icon: 3, title: '提示' }, function (index) {
+            // 清空本地 token 
+            localStorage.removeItem('token');
+            // 跳转页面
+            location.href = '/login.html'
+            // 关闭询问窗口
+            layer.close(index);
+        })
+    })
+
 });
 
 // 获取用户信息，封装到入口函数外面
@@ -12,10 +26,10 @@ function getUserInfo() {
     // 发送请求
     $.ajax({
         url: '/my/userinfo',
-        headers: {
-            // 获取登录
-            Authorization: localStorage.getItem("token") || "",
-        },
+        // headers: {
+        //     // 获取登录
+        //     Authorization: localStorage.getItem("token") || "",
+        // },
         success: (res) => {
             console.log(res.data);
             if (res.status != 0) {
